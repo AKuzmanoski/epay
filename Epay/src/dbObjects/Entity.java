48473087e5b4781f1ddca20858a0b3c4e793923b
SociaLLabel <aleksandarkuzmanoski11@gmail.com>
2015-05-25 23:16:05 +0200

    Samo patekata kaj mene

--- Epay/src/dbObjects/Entity.java
+++ Epay/src/dbObjects/Entity.java
@@ -14,6 +14,7 @@ public class Entity {
 	IllegalAccessException, ClassNotFoundException, IOException {
 		// passwords shouldn't be hard-coded into the code, a better practice is to read them from file
 		//String passLoc = "C:/Users/Angela/dbpass.txt";
+		//String passLoc = "C:/Users/User/Desktop/MyLearningMaterials/TretaGodina/SestiSemestar/SystemSecurity/Proekt/Password.txt";
 		String passLoc = "/home/goran/dbpass";
 		BufferedReader br = new BufferedReader(new FileReader(passLoc));
 		String pass = br.readLine();
