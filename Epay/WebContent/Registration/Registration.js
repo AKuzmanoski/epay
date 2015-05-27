79ff29b182f0bf54e923e820a81de870940f7b9b
SociaLLabel <aleksandarkuzmanoski11@gmail.com>
2015-05-27 15:52:54 +0200

    Forma za registracija

---
+++ Epay/WebContent/Registration/Registration.js
@@ -0,0 +1,269 @@
+/**
+ * 
+ */
+
+$(document).ready(function() {
+	$(".date").datepicker({
+		altField : "#actualDate"
+	});
+
+	$("input[type=button]").button({});
+	$("input[type=submit]").button({});
+
+	$("#next").click(function() {
+		if (isValidUserInfo()) {
+			$("#personalInfo").toggle();
+			$("#accountInfo").toggle();
+		}
+	});
+
+	$("#back").click(function() {
+		$("#personalInfo").toggle();
+		$("#accountInfo").toggle();
+	});
+
+	$("#individual").attr("checked", true);
+
+	$("#individual").click(function() {
+		if ($(this).is(":checked")) {
+			$("#individualForm").css("display", "block");
+			$("#firmForm").css("display", "none");
+		}
+	});
+
+	$("#firm").click(function() {
+		if ($(this).is(":checked")) {
+			$("#individualForm").css("display", "none");
+			$("#firmForm").css("display", "block");
+		}
+	});
+	
+	$("form").submit(function(event) {
+		if (!validate()) {
+			if(event.preventDefault) event.preventDefault(); else event.returnValue = false;
+		}
+	});
+	
+	$(".required").blur(function() {
+		isEmptyValidator($(this));
+	});
+	
+	$("#accountnumber").blur(function() {
+		validateAccountNumber($(this));
+	});
+	
+	$("#datefrom").blur(function() {
+		isValidDate($(this));
+	});
+	
+	$("#dateto").blur(function() {
+		isValidDate($(this));
+	});
+	
+	$("#confirm").blur(function() {
+		isEqualValidator($(this), $("#password"));
+	})
+	
+	$("#dateofbirth").blur(function() {
+		isValidDate($(this));
+	});
+	
+	$("#email").blur(function() {
+		validateEmail($(this))
+	});
+});
+
+function validate() {
+	return isValidUserInfo() && isValidAccountInfo();
+}
+
+function isValidAccountInfo() {
+	var isValid = true;
+	isValid = validateAccountNumber($("#accountnumber")) && isValid;
+	isValid = isEmptyValidator($("#datefrom")) && isValid;
+	isValid = isValidDate($("#datefrom")) && isValid;
+	isValid = isEmptyValidator($("#dateto")) && isValid;
+	isValid = isValidDate($("#dateto")) && isValid;
+	
+	return isValid;
+}
+
+function isValidUserInfo() {
+	isValid = true;
+	isValid = isEmptyValidator($("#username")) && isValid;
+	isValid = isEmptyValidator($("#password")) && isValid;
+	isValid = isEqualValidator($("#confirm"), $("#password")) && isValid;
+	if ($("#individual").is(":checked")) {
+		isValid = isEmptyValidator($("#firstname")) && isValid;
+		isValid = isEmptyValidator($("#lastname")) && isValid;
+		isValid = isEmptyValidator($("#socialsecurity")) && isValid;
+		isValid = isValidDate($("#dateofbirth")) && isValid;
+	} else {
+		isValid = isEmptyValidator($("#companyname")) && isValid;
+	}
+	isValid = isEmptyValidator($("#email")) && isValid;
+	isValid = validateEmail($("#email")) && isValid;
+	return isValid;
+}
+
+function isEqualValidator(field1, field2) {
+	if(field1.val() == field2.val()) {
+		field1.next().css("display", "none");
+		return true;
+	} else {
+		field1.next().css("display", "inline");
+		return false;
+	}
+}
+
+function regexValidator(reg, field) {
+	if (!reg.test(field.val())) {
+		field.next().css("display", "inline");
+		return false;
+	} else {
+		field.next().css("display", "none");
+		return true;
+	}
+}
+
+function isEmptyValidator(field) {
+	return regexValidator(/.+/, field);
+}
+
+function isValidDate(field) {
+	d = new Date(field.val());
+	if (isNaN(d.getTime())) { // d.valueOf() could also work
+		// date is not valid
+		field.next().css("display", "inline");
+		return false;
+	} else {
+		// date is valid
+		field.next().css("display", "none");
+		return true;
+	}
+}
+
+function validateEmail(field) {
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    result = re.test(field.val());
+    if (result) {
+    	field.next().css("display", "none");
+		return true;
+    } else {
+    	field.next().css("display", "inline");
+		return false;
+    }
+}
+
+function validateAccountNumber(field) {
+	if (isEmptyValidator(field)) {
+		field.next().css("display", "none");
+		return true;
+	} else {
+		field.next().css("display", "inline");
+		return false;
+	}
+}
+
+function isIbanValid(value) {
+
+    var lengthMap = getLengthMap();
+
+    //cleanup
+    value = value.toString().toUpperCase().replace(/\s/g, '').replace(/[-]/g, '');
+    //check if alphanumeric
+    if (!/^[a-zA-Z0-9]+$/.test(value)) return false;
+    //extract countrycode
+    var countryCode = value.substring(0, 2);
+    //check if letter
+    if (!/([a-z]+[\s\-]?)*/i.test(countryCode)) return false;
+    //check string length
+    if (value.length != lengthMap[countryCode]) return false;
+
+    value = value.concat(value.substring(0, 4)).substring(4);
+    value = value.replace(countryCode, countryCodeToStringValue(countryCode));
+
+    return modulo(value, 97) == 1;
+
+    function countryCodeToStringValue(countryCode) {
+        return "".concat(((countryCode.charCodeAt(0)) - 55).toString() + (countryCode.charCodeAt(1) - 55).toString());
+    }
+
+    function modulo(divident, divisor) {
+        var quantization = 12;
+        while (quantization < divident.length) {
+            var part = divident.substring(0, quantization);
+            divident = (part % divisor) + divident.substring(quantization);
+        }
+        return divident % divisor;
+    }
+
+    function getLengthMap() {
+        var lengthMap = {};
+        lengthMap["AD"] = 24;
+        lengthMap["AT"] = 20;
+        lengthMap["AZ"] = 28;
+        lengthMap["BH"] = 22;
+        lengthMap["BE"] = 16;
+        lengthMap["BA"] = 20;
+        lengthMap["BR"] = 29;
+        lengthMap["BG"] = 22;
+        lengthMap["CR"] = 21;
+        lengthMap["HR"] = 21;
+        lengthMap["CY"] = 28;
+        lengthMap["CZ"] = 24;
+        lengthMap["DK"] = 18;
+        lengthMap["DO"] = 28;
+        lengthMap["EE"] = 20;
+        lengthMap["FO"] = 18;
+        lengthMap["FI"] = 18;
+        lengthMap["FR"] = 27;
+        lengthMap["DE"] = 22;
+        lengthMap["GR"] = 27;
+        lengthMap["GI"] = 23;
+        lengthMap["GL"] = 18;
+        lengthMap["GT"] = 28;
+        lengthMap["HU"] = 28;
+        lengthMap["IS"] = 26;
+        lengthMap["IE"] = 22;
+        lengthMap["IL"] = 23;
+        lengthMap["IT"] = 27;
+        lengthMap["JO"] = 30;
+        lengthMap["KZ"] = 20;
+        lengthMap["KW"] = 30;
+        lengthMap["LV"] = 21;
+        lengthMap["LB"] = 28;
+        lengthMap["LI"] = 21;
+        lengthMap["LT"] = 20;
+        lengthMap["LU"] = 20;
+        lengthMap["MK"] = 19;
+        lengthMap["MT"] = 31;
+        lengthMap["MR"] = 27;
+        lengthMap["MU"] = 30;
+        lengthMap["MC"] = 27;
+        lengthMap["MD"] = 24;
+        lengthMap["ME"] = 22;
+        lengthMap["NL"] = 18;
+        lengthMap["NO"] = 15;
+        lengthMap["PK"] = 24;
+        lengthMap["PS"] = 29;
+        lengthMap["PL"] = 28;
+        lengthMap["PT"] = 25;
+        lengthMap["QA"] = 29;
+        lengthMap["RO"] = 24;
+        lengthMap["SM"] = 27;
+        lengthMap["SA"] = 24;
+        lengthMap["RS"] = 22;
+        lengthMap["SK"] = 24;
+        lengthMap["SI"] = 19;
+        lengthMap["ES"] = 24;
+        lengthMap["SE"] = 24;
+        lengthMap["CH"] = 21;
+        lengthMap["TN"] = 24;
+        lengthMap["TR"] = 26;
+        lengthMap["AE"] = 23;
+        lengthMap["GB"] = 22;
+
+        return lengthMap;
+    }
+}
---
+++ Epay/WebContent/Registration/userRegistration.jsp
@@ -0,0 +1,176 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>Registration</title>
+	<link type="text/css" rel="stylesheet" href="Registration.css"></link>
+	<link type="text/css" rel="stylesheet" href="../jQuery/jquery-ui.min.css"></link>
+	
+	
+	<script type="text/javascript" src="../jQuery/jquery-2.1.4.min.js"></script>
+	<script type="text/javascript" src="../jQuery/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="Registration.js"></script>	
+	</head>
+	<body>
+		<div id="content" class="content">
+			<h3>Sign Up</h3>
+			<form action="SignupServlet" name="registration" id="registration" method="post">
+				<div id="personalInfo">
+					<table>
+						<tr>
+							<td>Username</td>
+							<td>
+								<input type="text" id="username" name="username" placeholder="Your unique username" class="required" />
+								<img id="usernameErr" class="err" src="Images/error.png" title="This field should not be empty or taken by another user." />
+							</td>
+						</tr>
+						<tr>
+							<td>Password</td>
+							<td><input type="password" id="password" name="password" class="required" />
+							<img id="passwordErr" class="err" src="Images/error.png" title="This field is required." />
+							</td>
+						</tr>
+						<tr>
+							<td>Confirm Password</td>
+							<td><input type="password" id="confirm" />
+							<img id="confirmErr" class="err" src="Images/error.png" title="This field is required and should have the same content as previous one." />
+							</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input type="radio" id="individual" name="isIndividual" value="individual" />
+								Individual <input type="radio" id="firm" name="isIndividual" value="firm" />
+								Firm</td>
+						</tr>
+					</table>
+					<hr />
+					<div id="individualForm" class="Forms">
+						<table>
+							<tr>
+								<td>First Name</td>
+								<td><input type="text" id="firstname" name="firstname" placeholder="Your First Name" class="required" />
+								<img id="firstnameErr" class="err" src="Images/error.png" title="This field is required." />
+								</td>
+							</tr>
+							<tr>
+								<td>Last Name</td>
+								<td><input type="text" id="lastname" name="lastname" placeholder="Your Last Name" class="required" />
+								<img id="lastnameErr" class="err" src="Images/error.png" title="This field is required." />
+								</td>
+							</tr>
+							<tr>
+								<td>Social Security</td>
+								<td><input type="text" id="socialsecurity"
+									name="socialsecurity" placeholder="Your unique Social Security Identifier" class="required" />
+									<img id="socialsecurityErr" class="err" src="Images/error.png" title="This field is required." />
+									</td>
+							</tr>
+							<tr>
+								<td>Date Of Birth</td>
+								<td><input type="text" id="dateofbirth" name="dateofbirth" class="date" placeholder="Choose your Date of Birth" />
+								<img id="dateofbirthErr" class="err" src="Images/error.png" title="Please use standard date format (mm/dd/yyyy) or choose from dropdown chooser." />
+								</td>
+							</tr>
+						</table>
+					</div>
+					<div id="firmForm" class="Forms">
+						<table>
+							<tr>
+								<td>Company Name</td>
+								<td><input type="text" id="companyname" name="companyname" placeholder="Official Company Name" class="required" />
+								<img id="companynameErr" class="err" src="Images/error.png" title="This field is required." /></td>
+							</tr>
+						</table>
+					</div>
+					<hr />
+					<table>
+						<tr>
+							<td>Email</td>
+							<td><input type="text" id="email" name="email" placeholder="An active Email address" />
+							<img id="emailErr" class="err" src="Images/error.png" title="This field is required and the standard email format is required" /></td>
+						</tr>
+						<tr>
+							<td>Contact Number</td>
+							<td><input type="text" id="contactnumber" name="contactnumber" placeholder="An active phone number" /></td>
+						</tr>
+						<tr>
+							<td>Country</td>
+							<td><select id="country" name="country">
+									<option>Macedonia</option>
+									<option>Albania</option>
+									<option>Serbia</option>
+									<option>Montenegro</option>
+									<option>Croatia</option>
+									<option>Bulgaria</option>
+							</select></td>
+						</tr>
+						<tr>
+							<td>City</td>
+							<td><select id="city" name="city">
+									<option>Gostivar</option>
+									<option>Skopje</option>
+									<option>Bitola</option>
+									<option>Kocani</option>
+									<option>Gevgelija</option>
+									<option>Tetovo</option>
+							</select></td>
+						</tr>
+						<tr>
+							<td>Address</td>
+							<td><input type="text" id="address" name="address" placeholder="Your current address" /></td>
+						</tr>
+					</table>
+					<hr />
+					<table>
+						<tr>
+							<td></td>
+							<td class="navButtons" id="navFirst"><input type="button" id="next" value="Next" /></td>
+						</tr>
+					</table>
+				</div>
+				<div id="accountInfo">
+					<table>
+						<tr>
+							<td>Bank</td>
+							<td>
+								<select id="bank" name="bank" >
+									<option>Stopanska Banka AD</option>
+									<option>Tutunska Banka AD</option>
+									<option>Ohridska Banka</option>
+									<option>Halk Bank</option>
+									<option>Sparkase Bank</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td>Account Number</td>
+							<td><input type="text" id="accountnumber" name="accountnumber" placeholder="Your unique Bank Account Number" />
+							<img id="accountnumberErr" class="err" src="Images/error.png" title="This field is required." /></td>
+						</tr>
+						<tr>
+							<td>Date From</td>
+							<td><input type="text" id="datefrom" name="datefrom" class="date" placeholder="Date of Issue" />
+							<img id="socialsecurityErr" class="err" src="Images/error.png" title="This field is required. Please use standard date format (mm/dd/yyyy) or choose from dropdown chooser." /></td>
+						</tr>
+						<tr>
+							<td>Date From</td>
+							<td><input type="text" id="dateto" name="dateto" class="date" placholder="Date of Expiry" />
+							<img id="socialsecurityErr" class="err" src="Images/error.png" title="This field is required. Please use standard date format (mm/dd/yyyy) or choose from dropdown chooser." /></td>
+						</tr>
+					</table>
+					<hr />
+					<table>
+						<tr>
+							<td></td>
+							<td class="navButtons" id="navFirst">
+								<input type="button" name="back" id="back" value="Back" />
+								<input type="submit" name="sub" id="sub" value="Submit" />
+							</td>
+						</tr>
+					</table>
+				</div>
+			</form>
+		</div>
+	</body>
+</html>
